cbcde3c2a57957b2a008344cf77284200f9d6fe7
alanj <alanjoshua2002@gmail.com>
2020-04-15 01:01:57 -0400

    updated IPS system to take actual radial distances

--- RenderEngine/src/ENED_Simulation/Robot.java
+++ RenderEngine/src/ENED_Simulation/Robot.java
@@ -98,10 +98,60 @@ public class Robot extends Movable {
             radVals[i] = Float.parseFloat(split[i]);
         }
 
-        radVals[2] *= -1;
+        // Hidden method to call teleport option
+        if(split.length > 3) {
+            System.out.println("called teleport command. Teleporting to specified location...");
+            radVals[2] *= -1;
+            this.setPos(new Vector(radVals));
+        }
 
-        this.setPos(new Vector(radVals));
-        System.out.println("coordinates logged");
+        else if(split.length == 3) {
+
+//            Commented below is a method to calculate coords only from A and C. Works because we know for a fact that
+//            in this simulation the robot will never go out of bounds
+
+//            float a = radVals[0];
+//            float c = radVals[1];
+//            float d = radVals[2];
+//            Vector C = new Vector(new float[]{boundMin.get(0),boundMax.get(1)});
+//            float ac = C.sub(boundMin).getNorm();
+//
+//            float angleA = (float) Math.cos((-(c*c) + (ac*ac) + (a*a)) / (2*ac*a));
+//            float angleAComp = (float) ((Math.PI/2) - angleA);
+//
+//            Vector preFinal = new Vector(new float[]{(float) Math.cos(angleAComp), (float) Math.sin(angleAComp)}).scalarMul(a);
+//            System.out.println("Calculated coordinates are: ");
+//            Vector res = new Vector(new float[]{preFinal.get(0),pos.get(1),preFinal.get(1)});
+//            res.display();
+//            System.out.println("Actual coordinates: ");
+//            Vector tempPos = new Vector(new float[]{pos.get(0),pos.get(1),pos.get(2)*-1});
+//            tempPos.display();
+
+            float x1,x2,x3,y1,y2,y3,r1,r2,r3,x,y;
+            x1 = boundMin.get(0);
+            y1 = boundMin.get(1);
+            r1 = radVals[0];
+
+            x2 = boundMin.get(0);
+            y2 = boundMax.get(1);
+            r2 = radVals[1];
+
+            x3 = boundMax.get(0);
+            y3 = boundMax.get(1);
+            r3 = radVals[2];
+
+//            Triangulation formula from ENED community site
+            x = (-(y2 - y3)*(((y2*y2)-(y1*y1))+((x2*x2)-(x1*x1))+((r1*r1)-(r2*r2))) + (y1-y2)*(((y3*y3)-(y2*y2))+((x3*x3)-(x2*x2))+((r2*r2)-(r3*r3))))/(2*((x1-x2)*(y2-y3) - (x2-x3)*(y1-y2)));
+            y = (-(x2 - x3)*(((x2*x2) - (x1*x1)) + ((y2*y2) - (y1*y1)) + ((r1*r1) - (r2*r2)))+((x1-x2)*(((x3*x3)-(x2*x2))+((y3*y3)-(y2*y2))+((r2*r2)-(r3*r3)))))/(2*((y1-y2)*(x2-x3) - (y2-y3)*(x1-x2)));
+
+            System.out.println("Calculated coordinates are: ");
+            Vector res = new Vector(new float[]{x,pos.get(1),y});
+            res.display();
+            System.out.println("Actual coordinates: ");
+            pos.display();
+
+        }
+
     }
 
 }
