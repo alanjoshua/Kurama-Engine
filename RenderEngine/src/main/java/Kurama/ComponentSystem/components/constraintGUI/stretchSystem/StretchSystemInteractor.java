00abfdb3c801febcc499ee55e51306cd96a094ba
alanjoshua <41203962+alanjoshua@users.noreply.github.com>
2021-06-07 21:57:39 +0530

    Working on stretch bound secondary movement validification

--- RenderEngine/src/main/java/Kurama/ComponentSystem/components/constraintGUI/stretchSystem/StretchSystemInteractor.java
+++ RenderEngine/src/main/java/Kurama/ComponentSystem/components/constraintGUI/stretchSystem/StretchSystemInteractor.java
@@ -52,24 +52,125 @@ public class StretchSystemInteractor implements Interactor {
         // most likely being stretched by the stretch system or the rigid system
         else if(boundary.boundaryOrient == Boundary.BoundaryOrient.Horizontal && info.deltaMoveX!=0) {
 
-            boundary.updatedWidth = boundary.width + (-relativePos * info.deltaMoveX);
-            boundary.updatedPos = boundary.pos.add(new Vector(info.deltaMoveX/2f, 0, 0));
+            // Add check here to see whether it should remain fixed or not
 
-            boundary.shouldUpdateWidth = true;
-            boundary.shouldUpdatePos = true;
+            if(isSecondaryHorizontalBoundMoveValid(boundary, parentBoundary, info.deltaMoveY, relativePos)) {
+                boundary.updatedWidth = boundary.width + (-relativePos * info.deltaMoveX);
+                boundary.updatedPos = boundary.pos.add(new Vector(info.deltaMoveX / 2f, 0, 0));
+
+                boundary.shouldUpdateWidth = true;
+                boundary.shouldUpdatePos = true;
+            }
         }
 
         // most likely being stretched by the stretch system or the rigid system
         else if(boundary.boundaryOrient == Boundary.BoundaryOrient.Vertical && info.deltaMoveY!=0) {
 
-            boundary.updatedHeight = boundary.height + relativePos * info.deltaMoveY;
-            boundary.updatedPos = boundary.pos.add(new Vector(0, info.deltaMoveY/2f, 0));
+            // Add check here to see whether it should remain fixed or not
+            if(isSecondaryVertBoundMoveValid(boundary, parentBoundary, info.deltaMoveY, relativePos)) {
+                boundary.updatedHeight = boundary.height + relativePos * info.deltaMoveY;
+                boundary.updatedPos = boundary.pos.add(new Vector(0, info.deltaMoveY / 2f, 0));
 
-            boundary.shouldUpdateHeight = true;
-            boundary.shouldUpdatePos = true;
+                boundary.shouldUpdateHeight = true;
+                boundary.shouldUpdatePos = true;
+            }
         }
 
         return true;
+    }
+
+    public boolean isSecondaryVertBoundMoveValid(Boundary boundary, Boundary parentBoundary, float deltaMoveY, int relativePos) {
+
+        var tentativePos = boundary.pos.add(new Vector(0, deltaMoveY/2f, 0));
+        var tentativeHeight = boundary.height + relativePos * deltaMoveY;
+
+        boolean isAnyHBoundLeftHanging = false;
+
+        for(var b: boundary.positiveAttachments) {
+            var bPos = b.shouldUpdatePos?b.updatedPos:b.pos;
+
+            if(deltaMoveY >= 0) { // Moving down
+                if(bPos.geti(1) < tentativePos.geti(1)) {
+                    isAnyHBoundLeftHanging = true;
+                    break;
+                }
+            }
+            else {
+                if(bPos.geti(1) > tentativePos.geti(1)) {
+                    isAnyHBoundLeftHanging = true;
+                    break;
+                }
+            }
+        }
+
+        if(!isAnyHBoundLeftHanging) {
+            for (var b : boundary.negativeAttachments) {
+                var posOfBoundBeingComparedTo = b.shouldUpdatePos ? b.updatedPos : b.pos;
+
+                if (deltaMoveY >= 0) { // Moving down
+                    if (posOfBoundBeingComparedTo.geti(1) < tentativePos.geti(1)) {
+                        isAnyHBoundLeftHanging = true;
+                        break;
+                    }
+                } else {
+                    if (posOfBoundBeingComparedTo.geti(1) > tentativePos.geti(1)) {
+                        isAnyHBoundLeftHanging = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    public boolean isSecondaryHorizontalBoundMoveValid(Boundary boundary, Boundary parentBoundary, float deltaMoveX, int relativePos) {
+
+        var tentativeWidth = boundary.width + (-relativePos * deltaMoveX);
+        var tentativePos = boundary.pos.add(new Vector(deltaMoveX/2f, 0, 0));
+
+        boolean isAnyHBoundLeftHanging = false;
+
+        for(var b: boundary.positiveAttachments) {
+            if(b == parentBoundary) continue;
+            var posOfBoundBeingComparedTo = b.shouldUpdatePos?b.updatedPos:b.pos;
+
+            if(deltaMoveX >= 0) { // Moving down
+                if(posOfBoundBeingComparedTo.geti(0) < tentativePos.geti(0)) {
+                    isAnyHBoundLeftHanging = true;
+                    break;
+                }
+            }
+            else {
+                if(posOfBoundBeingComparedTo.geti(0) > tentativePos.geti(0)) {
+                    isAnyHBoundLeftHanging = true;
+                    break;
+                }
+            }
+        }
+
+        if(!isAnyHBoundLeftHanging) {
+            for (var b : boundary.negativeAttachments) {
+                if(b == parentBoundary) continue;
+                var bPos = b.shouldUpdatePos ? b.updatedPos : b.pos;
+
+                if (deltaMoveX >= 0) { // Moving right
+                    if (bPos.geti(0) < tentativePos.geti(0)) {
+                        isAnyHBoundLeftHanging = true;
+                        break;
+                    }
+                } else {
+                    if (bPos.geti(0) > tentativePos.geti(0)) {
+                        isAnyHBoundLeftHanging = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return true;
+
     }
 
 }
--- RenderEngine/src/main/java/main/Main.java
+++ RenderEngine/src/main/java/main/Main.java
@@ -13,8 +13,8 @@ public class Main {
 	}
 	
 	public void start() {
-		game = new GameLWJGL("OpenGL Renderer");
-//		game = new Editor("Editor");
+//		game = new GameLWJGL("OpenGL Renderer");
+		game = new Editor("Editor");
 		game.start();
 
 	}
