cb6e4ce632ccaf7be8af3f98ee53a939a0ae6cc4
alanjoshua <41203962+alanjoshua@users.noreply.github.com>
2022-09-01 17:29:40 -0400

    Created branch

---
+++ RenderEngine/projects/VulkanTestProject/code/main/MultiViewGame.java
@@ -0,0 +1,34 @@
+package main;
+
+import Kurama.game.Game;
+
+public class MultiViewGame extends Game {
+
+    public MultiViewGame(String threadName) {
+        super(threadName);
+    }
+
+    @Override
+    public void init() {
+        initVulkan();
+    }
+
+    public void initVulkan() {
+
+    }
+
+    @Override
+    public void cleanUp() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
---
+++ RenderEngine/projects/VulkanTestProject/code/main/MultiViewRenderer.java
@@ -0,0 +1,26 @@
+package main;
+
+import Kurama.game.Game;
+import Kurama.renderingEngine.RenderingEngine;
+import Kurama.scene.Scene;
+
+public class MultiViewRenderer extends RenderingEngine {
+
+    public MultiViewRenderer(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void init(Scene scene) {
+
+    }
+
+    public void initVulkan() {
+
+    }
+
+    @Override
+    public void cleanUp() {
+
+    }
+}
