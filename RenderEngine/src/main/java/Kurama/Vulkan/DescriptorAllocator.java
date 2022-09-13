7bbbf6ec4d55b6466141b3b0b2bdace5182a865e
alanj <alanjoshua2002@gmail.com>
2022-09-13 11:50:56 -0400

    partial commit, created descriptor layout cache

--- RenderEngine/src/main/java/Kurama/Vulkan/DescriptorAllocator.java
+++ RenderEngine/src/main/java/Kurama/Vulkan/DescriptorAllocator.java
@@ -1,26 +1,138 @@
 package Kurama.Vulkan;
 
-import org.lwjgl.vulkan.VkDescriptorPoolSize;
-import org.lwjgl.vulkan.VkDescriptorSetLayoutBinding;
-import org.lwjgl.vulkan.VkDevice;
+import org.lwjgl.vulkan.*;
 
+import java.nio.LongBuffer;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
+import static Kurama.Vulkan.VulkanUtilities.device;
+import static Kurama.Vulkan.VulkanUtilities.vkCheck;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.vulkan.VK10.*;
 import static org.lwjgl.vulkan.VK10.VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+import static org.lwjgl.vulkan.VK11.VK_ERROR_OUT_OF_POOL_MEMORY;
 
 public class DescriptorAllocator {
 
     VkDevice device;
-    List<Long> freePools;
-    List<Long> usedPools;
+    LinkedList<Long> freePools = new LinkedList<>();
+    LinkedList<Long> usedPools = new LinkedList<>();
 
+    private Long currentPool = null;
 
-    private long currentPool;
+    public HashMap<Integer, Float> poolSizes = new HashMap<>();
+
+    public DescriptorAllocator() {
+        poolSizes.put(VK_DESCRIPTOR_TYPE_SAMPLER, 0.5f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 4f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 4f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 2f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 2f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1f);
+        poolSizes.put(VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 0.5f);
+    }
+
+    public DescriptorAllocator(HashMap<Integer, Float> poolSizes) {
+        this.poolSizes = poolSizes;
+    }
 
     public void init(VkDevice device) {
         this.device = device;
+    }
+
+    public Long allocate(long descriptorSetLayout) {
+
+        try (var stack = stackPush()) {
+
+            if (currentPool == null) {
+                currentPool = grabPool();
+                usedPools.add(currentPool);
+            }
+
+            var allocinfo = VkDescriptorSetAllocateInfo.calloc(stack);
+            allocinfo.sType(VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO);
+            allocinfo.descriptorPool(currentPool);
+            allocinfo.pSetLayouts(stack.longs(descriptorSetLayout));
+
+            var pDescriptorSet = stack.mallocLong(1);
+            var result = vkAllocateDescriptorSets(device, allocinfo, pDescriptorSet);
+
+            if(result == VK_SUCCESS) {
+                return pDescriptorSet.get(0);
+            }
+
+            // allocate new pool and reallocate descriptor set
+            else if(result == VK_ERROR_FRAGMENTED_POOL || result == VK_ERROR_OUT_OF_POOL_MEMORY) {
+
+                currentPool = grabPool();
+                usedPools.add(currentPool);
+                allocinfo.descriptorPool(currentPool);
+
+                // throw error if allocation still fails
+                vkCheck(vkAllocateDescriptorSets(device, allocinfo, pDescriptorSet));
+
+                return pDescriptorSet.get(0);
+            }
+
+            // for all other results, throw
+            else {
+                throw new RuntimeException("Failed to allocate descriptor set");
+            }
+
+        }
+
+    }
+
+    private long grabPool() {
+
+        if(freePools.size() > 0) {
+            return freePools.removeLast();
+        }
+
+        else {
+            return createPool(device, poolSizes, 1000, 0);
+        }
+    }
+
+    public void resetPools() {
+
+        for(var pool: usedPools) {
+            vkResetDescriptorPool(device, pool, 0);
+            freePools.add(pool);
+        }
+
+        usedPools.clear();
+        currentPool = null;
+    }
+
+    public long createPool(VkDevice device, HashMap<Integer, Float> poolSizes, int count, int descriptorPoolCreateFlags ) {
+        try (var stack = stackPush()) {
+            VkDescriptorPoolSize.Buffer creationPoolSizes = VkDescriptorPoolSize.calloc(poolSizes.size(), stack);
+
+            var keys = poolSizes.keySet().toArray();
+            for(int i = 0; i < poolSizes.size(); i++) {
+                var type = (Integer) keys[i];
+                creationPoolSizes.get(i).type(type);
+                creationPoolSizes.get(i).descriptorCount((int) (poolSizes.get(type) * count));
+            }
+
+            VkDescriptorPoolCreateInfo poolInfo = VkDescriptorPoolCreateInfo.calloc(stack);
+            poolInfo.sType(VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO);
+            poolInfo.pPoolSizes(creationPoolSizes);
+            poolInfo.maxSets(count);
+            poolInfo.flags(descriptorPoolCreateFlags);
+
+            LongBuffer pDescriptorPool = stack.mallocLong(1);
+            vkCheck(vkCreateDescriptorPool(device, poolInfo, null, pDescriptorPool));
+
+            return pDescriptorPool.get(0);
+        }
     }
 
     public void cleanUp() {
@@ -30,18 +142,6 @@ public class DescriptorAllocator {
         for (var p : usedPools) {
             vkDestroyDescriptorPool(device, p, null);
         }
-    }
-
-    public void createPool() {
-        
-    }
-
-    public void resetPools() {
-
-    }
-
-    public long allocate(long descriptorSetLayout) {
-        return 0;
     }
 
 }
---
+++ RenderEngine/src/main/java/Kurama/Vulkan/DescriptorSetLayoutCache.java
@@ -0,0 +1,103 @@
+package Kurama.Vulkan;
+
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VkDescriptorSetLayoutBinding;
+import org.lwjgl.vulkan.VkDescriptorSetLayoutCreateInfo;
+import org.lwjgl.vulkan.VkDevice;
+
+import java.nio.LongBuffer;
+import java.util.HashMap;
+import java.util.List;
+
+import static Kurama.Vulkan.VulkanUtilities.vkCheck;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.vulkan.VK10.*;
+import static org.lwjgl.vulkan.VK10.VK_SUCCESS;
+
+public class DescriptorSetLayoutCache {
+
+    public VkDevice device;
+
+    private HashMap<List<DescriptorBinding>, Long> layoutCache = new HashMap<>();
+
+    public void init(VkDevice device) {
+        this.device = device;
+    }
+
+    public long createDescriptorLayout(List<DescriptorBinding> setBindings) {
+
+        var descriptorSetLayout = layoutCache.get(setBindings);
+        if(descriptorSetLayout == null) {
+
+            try (var stack = stackPush()) {
+
+                VkDescriptorSetLayoutBinding.Buffer bindings = VkDescriptorSetLayoutBinding.calloc(setBindings.size(), stack);
+                for (int i = 0; i < setBindings.size(); i++) {
+                    bindings.put(i, createDescriptorSetLayoutBinding(setBindings.get(i).binding, setBindings.get(i).descriptorType, setBindings.get(i).stageFlags, stack));
+                }
+
+                VkDescriptorSetLayoutCreateInfo layoutInfo = VkDescriptorSetLayoutCreateInfo.calloc(stack);
+                layoutInfo.sType(VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO);
+                layoutInfo.pBindings(bindings);
+
+                LongBuffer pDescriptorSetLayout = stack.mallocLong(1);
+                vkCheck(vkCreateDescriptorSetLayout(device, layoutInfo, null, pDescriptorSetLayout));
+                descriptorSetLayout = pDescriptorSetLayout.get(0);
+
+                layoutCache.put(setBindings, descriptorSetLayout);
+            }
+
+        }
+
+        return descriptorSetLayout;
+    }
+
+    public void cleanUp() {
+        for(var key: layoutCache.keySet()) {
+            vkDestroyDescriptorSetLayout(device, layoutCache.get(key), null);
+        }
+        layoutCache.clear();
+    }
+
+    public static VkDescriptorSetLayoutBinding createDescriptorSetLayoutBinding(int binding, int descriptorType, int stageFlags, MemoryStack stack) {
+        VkDescriptorSetLayoutBinding.Buffer bindings = VkDescriptorSetLayoutBinding.calloc(1, stack);
+
+        VkDescriptorSetLayoutBinding bufferBinding = bindings.get(0);
+        bufferBinding.binding(binding);
+        bufferBinding.descriptorCount(1);
+        bufferBinding.descriptorType(descriptorType);
+        bufferBinding.stageFlags(stageFlags);
+
+        return bufferBinding;
+    }
+
+    public static class DescriptorBinding {
+
+        int binding;
+        int descriptorType;
+        int stageFlags;
+
+        public DescriptorBinding(int binding, int descriptorType, int stageFlags) {
+            this.binding = binding;
+            this.descriptorType = descriptorType;
+            this.stageFlags = stageFlags;
+        }
+
+        @Override
+        public int hashCode() {
+            // Adapted from hashCode function for Java List
+            int hashCode = 1;
+            hashCode = 31*hashCode + Integer.valueOf(binding).hashCode();
+            hashCode = 31*hashCode + Integer.valueOf(descriptorType).hashCode();
+            hashCode = 31*hashCode + Integer.valueOf(stageFlags).hashCode();
+            return hashCode;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return this.hashCode() == o.hashCode();
+        }
+
+    }
+
+}
