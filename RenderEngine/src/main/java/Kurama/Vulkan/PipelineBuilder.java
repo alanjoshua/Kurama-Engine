0a7a69ba25d44cdbcc954f7dbf0874d5babb69ab
alanj <alanjoshua2002@gmail.com>
2022-09-27 14:13:35 -0400

    Mostly done creating PipelineBuilder class

--- RenderEngine/src/main/java/Kurama/Vulkan/PipelineBuilder.java
+++ RenderEngine/src/main/java/Kurama/Vulkan/PipelineBuilder.java
@@ -1,13 +1,23 @@
 package Kurama.Vulkan;
 
 import Kurama.Math.Vector;
-import org.lwjgl.vulkan.VkDevice;
-import org.lwjgl.vulkan.VkExtent2D;
-import org.lwjgl.vulkan.VkPipelineShaderStageCreateInfo;
+import main.ActiveShutterRenderer;
+import main.RenderingEngineVulkan;
+import org.lwjgl.vulkan.*;
 
 import javax.swing.text.View;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static Kurama.Vulkan.ShaderSPIRVUtils.ShaderKind.FRAGMENT_SHADER;
+import static Kurama.Vulkan.ShaderSPIRVUtils.ShaderKind.VERTEX_SHADER;
+import static Kurama.Vulkan.ShaderSPIRVUtils.compileShaderFile;
+import static Kurama.Vulkan.VulkanUtilities.createShaderModule;
+import static Kurama.Vulkan.VulkanUtilities.device;
+import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.vulkan.VK10.*;
 
 public class PipelineBuilder {
@@ -81,22 +91,228 @@ public class PipelineBuilder {
         }
 
     }
-
+    public record PipelineAndLayout(long pipelineLayout, long pipeline){};
 
     List<ShaderStageCreateInfo> shaderStages;
     VertexBindingDescription vertexBindingDescription;
     List<VertexAttributeDescription> vertexAttributeDescriptions;
-    InputAssemblyCreateInfo inputAssemblyCreateInfo;
+    InputAssemblyCreateInfo inputAssemblyCreateInfo = new InputAssemblyCreateInfo();
     ViewPort viewport;
     Scissor scissor;
-    PipelineRasterizationStateCreateInfo rasterizer;
-    PipelineDepthStencilStateCreateInfo depthStencil;
-    PipelineColorBlendStateCreateInfo colorBlendAttach;
+    PipelineRasterizationStateCreateInfo rasterizer = new PipelineRasterizationStateCreateInfo();
+    PipelineDepthStencilStateCreateInfo depthStencil = new PipelineDepthStencilStateCreateInfo();
+    PipelineColorBlendStateCreateInfo colorBlendAttach = new PipelineColorBlendStateCreateInfo();
     PipelineMultisampleStateCreateInfo multiSample;
     PushConstant pushConstant;
-    List<Long> descriptorSetLayouts;
+    long[] descriptorSetLayouts;
 
-    public static long build(VkDevice device, long renderPass) {
+    public PipelineAndLayout build(VkDevice device, long renderPass) {
+
+        if(viewport == null) {
+            throw new IllegalArgumentException("Viewport cannot be null");
+        }
+        if(scissor == null) {
+            throw new IllegalArgumentException("Scissor cannot be null");
+        }
+
+        try(var stack = stackPush()) {
+
+            // SHADER STAGES
+            var shaderStagesBuffer = VkPipelineShaderStageCreateInfo.calloc(shaderStages.size(), stack);
+            var shaderModules = new ArrayList<Long>();
+            var shaderSPIRVs = new ArrayList<ShaderSPIRVUtils.SPIRV>();
+
+            for(int i = 0; i < shaderStages.size(); i++) {
+
+                var shader = shaderStages.get(i);
+
+                var entryPoint = stack.UTF8(shader.entryPoint);
+                ShaderSPIRVUtils.ShaderKind shaderKind = null;
+                
+                switch (shader.ShaderType) {
+                    case VK_SHADER_STAGE_VERTEX_BIT:
+                        shaderKind = VERTEX_SHADER;
+                        break;
+                    case VK_SHADER_STAGE_FRAGMENT_BIT:
+                        shaderKind = FRAGMENT_SHADER;
+                        break;
+                }
+
+                if(shaderKind == null) {
+                    throw new IllegalArgumentException("Invalid shader type was passed in");
+                }
+                var shaderSPRIV = compileShaderFile(shader.shaderFile, shaderKind);
+                var shaderModule = createShaderModule(shaderSPRIV.bytecode(), device);
+                shaderModules.add(shaderModule);
+                shaderSPIRVs.add(shaderSPRIV);
+
+                var shaderStageInfo = shaderStagesBuffer.get(i);
+                shaderStageInfo.sType(VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO);
+                shaderStageInfo.stage(shader.ShaderType);
+                shaderStageInfo.module(shaderModule);
+                shaderStageInfo.pName(entryPoint);
+            }
+
+            // VERTEX INFO
+            var vertexInputInfo = VkPipelineVertexInputStateCreateInfo.calloc(stack);
+            vertexInputInfo.sType(VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO);
+
+            if(vertexBindingDescription == null) {
+
+                if(vertexAttributeDescriptions == null || vertexAttributeDescriptions.size() == 0) {
+                    throw new IllegalArgumentException("Vertex binding attributes cannot be null when binding description is present");
+                }
+
+                vertexInputInfo.pVertexBindingDescriptions(null);
+                vertexInputInfo.pVertexAttributeDescriptions(null);
+            }
+            else {
+                var bindingDescription =
+                        VkVertexInputBindingDescription.calloc(1);
+
+                bindingDescription.binding(vertexBindingDescription.binding);
+                bindingDescription.stride(vertexBindingDescription.stride);
+                bindingDescription.inputRate(vertexBindingDescription.inputRate);
+
+                vertexInputInfo.pVertexBindingDescriptions(bindingDescription);
+
+                var attributeDescriptions = VkVertexInputAttributeDescription.calloc(vertexAttributeDescriptions.size());
+                for(int i = 0; i < vertexAttributeDescriptions.size(); i++) {
+                    var attribInfo = vertexAttributeDescriptions.get(i);
+                    var attrib = attributeDescriptions.get(i);
+                    attrib.binding(attribInfo.binding);
+                    attrib.location(attribInfo.location);
+                    attrib.format(attribInfo.format);
+                    attrib.offset(attribInfo.offset);
+                }
+                attributeDescriptions.rewind();
+                vertexInputInfo.pVertexAttributeDescriptions(attributeDescriptions);
+            }
+
+            // ASSEMBLY STAGE
+            var inputAssembly = VkPipelineInputAssemblyStateCreateInfo.calloc(stack);
+            inputAssembly.sType(VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO);
+            inputAssembly.topology(inputAssemblyCreateInfo.topology);
+            inputAssembly.primitiveRestartEnable(inputAssemblyCreateInfo.primitiveRestartEnable);
+
+            // VIEWPORT and SCISSOR
+            VkViewport.Buffer viewportBuffer = VkViewport.calloc(1, stack);
+            viewportBuffer.x(viewport.x);
+            viewportBuffer.y(viewport.y);
+            viewportBuffer.width(viewport.width);
+            viewportBuffer.height(viewport.height);
+            viewportBuffer.minDepth(viewport.minDepth);
+            viewportBuffer.maxDepth(viewport.maxDepth);
+
+            VkRect2D.Buffer scissorBuffer = VkRect2D.calloc(1, stack);
+            scissorBuffer.offset(VkOffset2D.calloc(stack).set(scissor.offsetX, scissor.offsetY));
+            scissorBuffer.extent(scissor.extent);
+
+            VkPipelineViewportStateCreateInfo viewportState = VkPipelineViewportStateCreateInfo.calloc(stack);
+            viewportState.sType(VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO);
+            viewportState.pViewports(viewportBuffer);
+            viewportState.pScissors(scissorBuffer);
+
+            // ===> RASTERIZATION STAGE <===
+            var rasterizerInfo = VkPipelineRasterizationStateCreateInfo.calloc(stack);
+            rasterizerInfo.sType(VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO);
+            rasterizerInfo.depthClampEnable(rasterizer.depthClampEnable);
+            rasterizerInfo.rasterizerDiscardEnable(rasterizer.rasterizerDiscardEnable);
+            rasterizerInfo.polygonMode(rasterizer.polygonMode);
+            rasterizerInfo.lineWidth(rasterizer.lineWidth);
+            rasterizerInfo.cullMode(rasterizer.cullMode);
+            rasterizerInfo.frontFace(rasterizer.frontFace);
+            rasterizerInfo.depthBiasEnable(rasterizer.depthBiasEnable);
+
+            var depthStencilInfo = VkPipelineDepthStencilStateCreateInfo.calloc(stack);
+            depthStencilInfo.sType(VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO);
+            depthStencilInfo.depthTestEnable(depthStencil.depthTestEnable);
+            depthStencilInfo.depthWriteEnable(depthStencil.depthWriteEnable);
+            depthStencilInfo.depthCompareOp(depthStencil.depthCompareOp);
+            depthStencilInfo.depthBoundsTestEnable(depthStencil.depthBoundsTestEnable);
+            depthStencilInfo.minDepthBounds(depthStencil.minDepthBounds); // Optional
+            depthStencilInfo.maxDepthBounds(depthStencil.maxDepthBounds); // Optional
+            depthStencilInfo.stencilTestEnable(depthStencil.stencilTestEnable);
+
+            // ===> MULTISAMPLING <===
+
+            VkPipelineMultisampleStateCreateInfo multisampling = null;
+            if(multiSample != null) {
+                multisampling = VkPipelineMultisampleStateCreateInfo.calloc(stack);
+                multisampling.sType(VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO);
+                multisampling.sampleShadingEnable(multiSample.sampleShadingEnabled);
+                multisampling.rasterizationSamples(multiSample.rasterizationSamples);
+                multisampling.minSampleShading(multiSample.minSampleShading);
+            }
+
+            // ===> COLOR BLENDING <===
+            var colorBlendAttachment = VkPipelineColorBlendAttachmentState.calloc(1, stack);
+            colorBlendAttachment.colorWriteMask(colorBlendAttach.colorWriteMask);
+            colorBlendAttachment.blendEnable(colorBlendAttach.blendEnable);
+
+            var colorBlending = VkPipelineColorBlendStateCreateInfo.calloc(stack);
+            colorBlending.sType(VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO);
+            colorBlending.logicOpEnable(colorBlendAttach.logicOpEnable);
+            colorBlending.logicOp(colorBlendAttach.logicOp);
+            colorBlending.pAttachments(colorBlendAttachment);
+
+            var blendConstants = colorBlendAttach.blendConstants;
+            colorBlending.blendConstants(stack.floats(blendConstants.get(0), blendConstants.get(1), blendConstants.get(2), blendConstants.get(3)));
+
+            VkPipelineLayoutCreateInfo pipelineLayoutInfo = VkPipelineLayoutCreateInfo.calloc(stack);
+            pipelineLayoutInfo.sType(VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO);
+            pipelineLayoutInfo.pSetLayouts(stack.longs(descriptorSetLayouts));
+
+            // PUSH CONSTANTS
+            if(pushConstant != null) {
+                var pushConstantBuffer = VkPushConstantRange.calloc(1, stack);
+                pushConstantBuffer.offset(pushConstant.offset);
+                pushConstantBuffer.size(pushConstant.size);
+                pushConstantBuffer.stageFlags(pushConstant.stageFlags);
+                pipelineLayoutInfo.pPushConstantRanges(pushConstantBuffer);
+            }
+
+            // ===> PIPELINE LAYOUT CREATION <===
+            LongBuffer pPipelineLayout = stack.longs(VK_NULL_HANDLE);
+
+            if(vkCreatePipelineLayout(device, pipelineLayoutInfo, null, pPipelineLayout) != VK_SUCCESS) {
+                throw new RuntimeException("Failed to create pipeline layout");
+            }
+            var pipelineLayout = pPipelineLayout.get(0);
+
+            VkGraphicsPipelineCreateInfo.Buffer pipelineInfo = VkGraphicsPipelineCreateInfo.calloc(1, stack);
+            pipelineInfo.sType(VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO);
+            pipelineInfo.pStages(shaderStagesBuffer);
+            pipelineInfo.pVertexInputState(vertexInputInfo);
+            pipelineInfo.pInputAssemblyState(inputAssembly);
+            pipelineInfo.pViewportState(viewportState);
+            pipelineInfo.pRasterizationState(rasterizerInfo);
+            pipelineInfo.pColorBlendState(colorBlending);
+            pipelineInfo.layout(pipelineLayout);
+            pipelineInfo.renderPass(renderPass);
+            pipelineInfo.subpass(0);
+            pipelineInfo.basePipelineHandle(VK_NULL_HANDLE);
+            pipelineInfo.basePipelineIndex(-1);
+            pipelineInfo.pDepthStencilState(depthStencilInfo);
+
+            if(multiSample != null) {
+                pipelineInfo.pMultisampleState(multisampling);
+            }
+
+            LongBuffer pGraphicsPipeline = stack.mallocLong(1);
+
+            if(vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, pipelineInfo, null, pGraphicsPipeline) != VK_SUCCESS) {
+                throw new RuntimeException("Failed to create graphics pipeline");
+            }
+
+            var graphicsPipeline = pGraphicsPipeline.get(0);
+
+            // RELEASE RESOURCES
+            shaderModules.forEach(s -> vkDestroyShaderModule(device, s, null));
+            shaderSPIRVs.forEach(s -> s.free());
+
+            return new PipelineAndLayout(pipelineLayout, graphicsPipeline);
+        }
 
     }
 
